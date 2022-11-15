4766958edae5c0809e266d718082e6b49ee440c1
imjm <77835002+imjm@users.noreply.github.com>
2022-11-16 02:42:04 +0900

    로그아웃 성공

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
-const {User} = require('../models/User');
+const { User } = require('../models/User');
+
 let auth = (req, res, next) => {
 
     // 미들웨어 처리하는 곳
@@ -8,16 +9,16 @@ let auth = (req, res, next) => {
     // 토큰을 복호화한 후 유저를 찾는다
     User.findByToken(token, (err, user) =>{
         if(err) throw err;
-        if(!user) return res.json({ isAuth: false, error: true})
+        if(!user) return res.json({ isAuth: false, error: true});
 
         req.token = token;
         req.user = user;
-        next()
-    })
+        next();
+    });
     // 유저가 있으면 인증 Okay
 
     // 유저가 없으면 인증 No
 
-}
+};
 
 module.exports = { auth };
